aa8fbbf141f3b8a50d1838f46b320dddb3d4ea21
MD ASHIKUR RAHMAN ASHIK <md.ashikur.upwork@gmail.com>
2026-05-04 15:52:32 +0000

    Adding comments to adjacency matrix

--- AdjacencyMatrix.cpp
+++ AdjacencyMatrix.cpp
@@ -1,33 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void initMatrix(int adjacencyMatrix[][6])
+void initMatrix(int adjacencyMatrix[][6])       // Received two dimensional matrix of size six from main function
 {
-    for (int i = 0; i < 6; i++)
+    for (int i = 0; i < 6; i++)         // Written loop for iterating over rows
     {
-        for (int j = 0; j < 6; j++)
+        for (int j = 0; j < 6; j++)     // Written loop for iterating over columns
         {
-            adjacencyMatrix[i][j] = 0;
+            adjacencyMatrix[i][j] = 0;  // Assigned initial value zero to each and every element of the adjacency matrix
         }
     }
 }
 
-void addEdge(int adjacencyMatrix[][6], int u, int v)
+void addEdge(int adjacencyMatrix[][6], int u, int v)        // Received two dimensional matrix, source node and destination node from main fucntion
 {
-    adjacencyMatrix[u][v] = 1;
-    adjacencyMatrix[v][u] = 1;
+    adjacencyMatrix[u][v] = 1;      // Assigned value one to the element which indicates a connection between source to node 
+    adjacencyMatrix[v][u] = 1;      // Assigned value one to the element which indicates a connection between node to source
 }
 
-void printMatrix(int adjacencyMatrix[][6])
+void printMatrix(int adjacencyMatrix[][6])      // Received adjacency matrix of size six
 {
-    for (int i = 0; i < 6; i++)
+    for (int i = 0; i < 6; i++)         // Written outer loop for iterating over rows
     {
-        for (int j = 0; i < 6; j++)
+        for (int j = 0; i < 6; j++)     // Written inner loop for iterating over columns
         {
-            cout << adjacencyMatrix[i][j] << " ";
+            cout << adjacencyMatrix[i][j] << " ";       // Printed each element in every iteration
         }
 
-        cout << "\n";
+        cout << "\n";                   // Added new line after printing every row
     }
 }
 
